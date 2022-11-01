9850729f75e276476ee94af37c40ad6d81affa52
Foureyes <87150906+dlsr00t@users.noreply.github.com>
2022-11-01 17:39:34 -0300

    Add files via upload

--- tst2.py
+++ tst2.py
@@ -8,24 +8,35 @@ import subprocess as sp
 
 class MyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
+        global tipo_evento
         
-        print('Evento', event.event_type,' caminho:'+ event.src_path+ 'diretorio?', event.is_directory)
-        with open("eventos.txt", "a") as eventos:
-            eventos.write(f'Evento {event.event_type} caminho: {event.src_path} diretorio? {event.is_directory}')
-            eventos.write('\n')
+        if event.src_path != '\\\\CAPSV\\Users\\Public\\RAFAEL\\Epic_Shielder-diretamente-do-lenovo-ideapad\\eventos.txt':
+            if event.event_type == "modified":
+                tipo_evento = "\033[33m modified \033[m"
+            elif event.event_type == "created":
+                tipo_evento = "\033[32m created \033[m"
+            elif event.event_type == "deleted":
+                tipo_evento = "\033[31m deleted \033[m"
 
-        print("fim da funcao")
-        global fim
-        fim = "acabou!"
-        global caminho
-        caminho = event.src_path
-        time.sleep(1)
+            print(f'Evento {tipo_evento} caminho: {event.src_path} diretorio? {event.is_directory}')
+            with open("eventos.txt", "a") as eventos:
+                eventos.write(f'Evento {event.event_type} caminho: {event.src_path} diretorio? {event.is_directory}')
+                eventos.write('\n')
+
+            
+            global fim
+            fim = "acabou!"
+            global caminho
+            caminho = event.src_path
+            time.sleep(1)
+            print("\033[34mtérmino da funcao\033[m")
 
 os.system('cls')if os.name == 'nt' else os.system('clear')
 
 path = input("Digite o caminho \033[4;33m[pressione enter para uso do caminho padrão]\033[m: ")
 if path == "" and os.name == "nt":
     path = "\\\\CAPSV\\Users\\Public\\RAFAEL\\Epic_Shielder-diretamente-do-lenovo-ideapad"
+    path = "\\\\CAPSV\\Users"
 else:    
     path = "/home/foureyes/.programs/python/sqlite"
 
@@ -35,9 +46,11 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 print('Rodando...')
+
 observer = Observer()
 observer.schedule(MyHandler(), path, recursive=True)
 observer.start()
+
 #observer.join()
 
 
@@ -45,18 +58,25 @@ try:
     while True:
         #print("aguardando")
         #time.sleep(1)
-        
+        '''
+        observer = Observer()
+        observer.schedule(MyHandler(), path, recursive=True)
+        observer.start()
+        '''
         try:
 
             if fim == "acabou!":
                     
-                    observer.stop()
+                    #observer.stop()
                     #os.system("dir")
                     variavel = sp.run(['dir'], shell=False, capture_output=True)
                     variavel = variavel.stdout
                     variavel = "".join(map(chr, variavel))
                     print(variavel)
-                    break
+                    
+        except KeyboardInterrupt:
+            observer.stop()
+            break
         except:
             pass
 
--- tst3.py
+++ tst3.py
@@ -5,6 +5,6 @@ import subprocess as sp
 variavel = sp.run(['dir'], shell=True, capture_output=True)
 variavel = variavel.stdout
 variavel = "".join(map(chr, variavel))
-print(variavel)
+print(f'\033[32m{variavel}\033[m')
 
 
